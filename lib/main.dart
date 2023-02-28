11acc6fdd3aa570c7ad48891df87832f488549f1
Difta Fitrahul Qihaj <88217314+DiftaFitrahul@users.noreply.github.com>
2023-02-28 18:03:59 +0700

    feat: finish initialize flutter firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gdsc_project/page/home_page.dart';
+import 'package:gdsc_project/firebase_options.dart';
 
-void main() {
+import '../page/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
